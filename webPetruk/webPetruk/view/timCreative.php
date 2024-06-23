4fb8f451065e277621f92ab27e360c20a1061fd9
Radhitya13 <radityanurrahaman@students.amikom.ac.id.>
2024-06-23 22:53:06 +0700

    first commit

---
+++ webPetruk/webPetruk/view/timCreative.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>PETRUK - Pengaduan Infrastruktur</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta content="" name="keywords" />
+  <meta content="" name="description" />
+
+  <!-- Google Web Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
+  <!-- Icon Font Stylesheet -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+
+  <!-- Libraries Stylesheet -->
+  <link href="../lib/animate/animate.min.css" rel="stylesheet" />
+  <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+
+  <!-- Customized Bootstrap Stylesheet -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet" />
+
+  <!-- Template Stylesheet -->
+  <link href="../css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <!-- Navbar Start -->
+  <?php
+  require_once 'user_header.php';
+  ?>
+  <!-- Navbar End -->
+
+
+  <!-- Content Start -->
+  <section id="creativeTim">
+    <div class="container-fluid main-content">
+      <div class="container">
+        <div class="header-ct py-lg-5 wow fadeInUp" data-wow-delay="0.3s">
+          <h1 class="text-primary text-center">CREATIVE TIM</h1>
+          <h5 class="text-primary text-center">KELOMPOK HABIT</h5>
+        </div>
+        <div class="body-ct ">
+          <div class="row pb-4">
+            <div class="col text-center">
+              <img class="fotoCT py-4 wow fadeInUp" data-wow-delay="0.5s" src="../img/1.jpg" alt="nanti diisi foto nya">
+              <h2 class="text-light wow fadeInUp" data-wow-delay="0.5s">Alivia Dyatama Arisusanto</h2>
+              <h4 class="text-primary wow fadeIn" data-wow-delay="0.7s">21.11.4123</h4>
+            </div>
+            <div class="col text-center">
+              <img class="fotoCT py-4 wow fadeInUp" data-wow-delay="1s" src="../img/radit (1).jpg" alt="nanti diisi foto nya">
+              <h2 class="text-light wow fadeInUp" data-wow-delay="1s">RADHITYA NUR RAHMAN</h2>
+              <h4 class="text-primary wow fadeInUp" data-wow-delay="1.2s">21.11.4175</h4>
+            </div>
+            <div class="col text-center">
+              <img class="fotoCT py-4 wow fadeInUp" data-wow-delay="1.5s" src="../img/hapid.png" alt="nanti diisi foto nya">
+              <h2 class="text-light wow fadeInUp" data-wow-delay="1.5s">HAFIDZ HARATMOKO</h2>
+              <h4 class="text-primary wow fadeIn" data-wow-delay="1.7s">21.11.4130</h4>
+            </div>
+            <div class="col text-center">
+              <img class="fotoCT py-4 wow fadeInUp" data-wow-delay="2s" src="../img/IMG_5389.JPG" alt="nanti diisi foto nya">
+              <h2 class="text-light wow fadeInUp" data-wow-delay="2s">Luthfia Ridho Damayanti</h2>
+              <h4 class="text-primary wow fadeIn" data-wow-delay="2.2s">21.11.4185</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="fcad201b-6c71-47e7-bead-6ea1aaa2fa4e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+  </section>
+  <!-- ----Content End----- -->
+
+
+  <!-- Footer Start -->
+  <?php
+  require_once 'footer.php';
+  ?>
+
+</body>
+
+</html>
